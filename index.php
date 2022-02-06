ad95dfc7e86c8f467ff63be7264037ad58d48994
youssefgamal11 <youssefgamal734@gmail.com>
2022-02-07 00:13:19 +0200

    task 1

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+ $units = 500 ;
+ if( $units <= 50){
+     echo 'electricity bill is ' ,$units*3.50;
+ }else if ($units > 50 && $units <= 150 ){
+     echo 'electricity bill is ' ,$units*4.00;
+ }else{
+     echo'electricity bill is ' , $units*6.50;
+ }
+
+
+?>
